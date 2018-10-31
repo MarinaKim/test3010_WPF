e99fdd4cf74de5603e0106255d9084ed6e9fd431
Марина <@DESKTOP-1C4J9CR>
2018-10-31 14:35:37 +0600

    Add project files.

---
+++ Students/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Students
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XDocument xdoc = new XDocument();
+           
+            XElement student1 = new XElement("students");          
+            XAttribute studNameAttr = new XAttribute("name", "Ivanov V.D.");
+            XElement studGroupElem = new XElement("group", "1");           
+            student1.Add(studNameAttr);
+            student1.Add(studGroupElem);
+
+            XElement student2 = new XElement("student");
+            XAttribute stud2NameAttr = new XAttribute("name", "Petrov.D.");
+            XElement stud2GroupElem = new XElement("group", "2");
+            student2.Add(stud2NameAttr);
+            student2.Add(stud2GroupElem);
+
+          
+            XElement students = new XElement("students");
+           
+            students.Add(student1);
+            students.Add(student2);
+            
+            xdoc.Add(students);
+            
+            xdoc.Save(@"C:\Users\Марина\Documents\visual studio 2015\Projects\Test3010_WPF\Students\students.xml");
+        }
+    }
+}
---
+++ Test3010_WPF/MainWindow.xaml.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Test3010_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private SqlConnection con;
+        private string SelectedTable = string.Empty;
+        private List<string> QueryColumns = new List<string>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            con = new SqlConnection();
+            con.ConnectionString = ConfigurationManager.ConnectionStrings["conStud"].ConnectionString;
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = "Select group_name, COUNT(stud_name) count_ from groups Join Studs on groups.group_id = Studs.group_id group by group_name order by count_";
+            SqlDataReader rd = cmd.ExecuteReader();
+
+            while (rd.Read())
+            {          
+                Label label = new Label();
+                Label label1 = new Label();
+                label.MouseDown += Label_MouseDown;
+                label.Content = rd[0].ToString();
+                label1.Content = rd[1].ToString();
+
+                spGroups.Children.Add(label);
+                spCount.Children.Add(label1);
+            }
+            rd.Close();
+            con.Dispose();
+        }
+
+        private void Label_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            tableRow.Items.Clear();
+               con = new SqlConnection();
+            con.ConnectionString = ConfigurationManager.ConnectionStrings["conStud"].ConnectionString;
+            con.Open();
+
+            Label label = (Label)sender;
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+
+            if (label.Content.ToString()== "group1")
+            {
+                cmd.CommandText = "Select stud_id,stud_name from Studs Join groups on groups.group_id = Studs.group_id where group_name = 'group1'";
+                SqlDataReader rd = cmd.ExecuteReader();
+
+                while (rd.Read())
+                {
+                    ListBoxItem itm = new ListBoxItem();                    
+                    itm.Content = rd[0].ToString() + " - " + rd[1].ToString(); 
+                    tableRow.Items.Add(itm);
+                }
+                rd.Close();              
+            }
+            else if (label.Content.ToString() == "group2")
+            {
+                cmd.CommandText = "Select stud_id,stud_name from Studs Join groups on groups.group_id = Studs.group_id where group_name = 'group2'";
+                SqlDataReader rd = cmd.ExecuteReader();
+
+                while (rd.Read())
+                {
+                    ListBoxItem itm = new ListBoxItem();
+                    itm.Content = rd[0].ToString() + " - " + rd[1].ToString(); ;
+
+                    tableRow.Items.Add(itm);
+                }
+                rd.Close();              
+            }
+            else if (label.Content.ToString() == "group3")
+            {
+                cmd.CommandText = "Select stud_id,stud_name from Studs Join groups on groups.group_id = Studs.group_id where group_name = 'group3'";
+                SqlDataReader rd = cmd.ExecuteReader();
+
+                while (rd.Read())
+                {
+                    ListBoxItem itm = new ListBoxItem();
+                    itm.Content = rd[0].ToString() + " - " + rd[1].ToString(); 
+
+                    tableRow.Items.Add(itm);
+                }
+                rd.Close();              
+            }
+            else if (label.Content.ToString() == "group4")
+            {
+                cmd.CommandText = "Select stud_id,stud_name from Studs Join groups on groups.group_id = Studs.group_id where group_name = 'group4'";
+                SqlDataReader rd = cmd.ExecuteReader();
+               
+                while (rd.Read())
+                {
+                    ListBoxItem itm = new ListBoxItem();
+                    itm.Content = rd[0].ToString() + " - " + rd[1].ToString(); 
+
+                    tableRow.Items.Add(itm);
+                }
+                rd.Close();              
+            }           
+            con.Dispose();           
+        }
+    }
+} 
+
